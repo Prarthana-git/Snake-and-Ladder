c8cf9ca966d932ee39026bdff0163fa0e3bc36af
Prarthana-git <prarthanachaudhari@gmail.com>
2021-05-25 16:08:10 +0530

    Starting position

---
+++ single.java
@@ -0,0 +1,11 @@
+public class single
+{
+  public static void main(String args[])
+  {
+   int Startposition=0;
+   int CurrentPosition=0;
+   System.out.println("Single player start position :" +Startposition);
+
+  }
+
+}
