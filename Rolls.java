57d756b46480820718700330af46d7acb261e99f
Prarthana-git <prarthanachaudhari@gmail.com>
2021-05-25 16:44:29 +0530

    Rolling dice

---
+++ Rolls.java
@@ -0,0 +1,12 @@
+public class Rolls
+ {
+   public static void main(String args[])
+   {
+    int startposition=0;
+   
+    int dice=(int)(Math.floor(Math.random()%6)+1);
+    System.out.println("Dice Face are:"+dice);
+
+   }
+
+ }
